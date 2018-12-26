06f44c6eaa3b351cbd3f96f7f4af25c2fa59f42e
caicai <jiangchong@qingclass.com>
2018-12-26 19:21:08 +0800

    error

--- cc/utils/collection/Collection.h
+++ cc/utils/collection/Collection.h
@@ -1,13 +1,13 @@
-#ifndef CC_COLLECTION_H
-#define CC_COLLECTION_H
+// #ifndef CC_COLLECTION_H
+// #define CC_COLLECTION_H
 
-namespace cc
-{
-template<typename T> class Collection
-{
-    bool add(T t);
-    bool remove(T t);
-};
+// namespace cc
+// {
+// template<typename T> class Collection
+// {
+//     bool add(T t);
+//     bool remove(T t);
+// };
 
-}; // namespace cc
-#endif
+// }; // namespace cc
+// #endif
